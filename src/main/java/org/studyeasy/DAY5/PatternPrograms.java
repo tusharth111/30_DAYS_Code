c31958e047ad27a5123fb8723efa9bc0190aba1a
Tushar Thakur <tusharth111@gamil.com>
2025-12-21 16:04:10 +0530

    learned how to add space and print double triangles

--- src/main/java/org/studyeasy/DAY5/PatternPrograms.java
+++ src/main/java/org/studyeasy/DAY5/PatternPrograms.java
@@ -8,7 +8,7 @@ package org.studyeasy.DAY5;
  ***/
 public class PatternPrograms {
     public static void main(String[] args) {
-        int n =4;
+        int n =5;
         pattern1(n);
         System.out.println();
         pattern2(n);
@@ -16,6 +16,12 @@ public class PatternPrograms {
         pattern3(n);
         System.out.println();
         pattern4(n);
+        System.out.println();
+        pattern5(n);
+        System.out.println();
+        pattern6(n);
+        System.out.println();
+        pattern7(n);
     }
     public static void pattern1(int n)
     {
@@ -29,7 +35,7 @@ public class PatternPrograms {
     public static void pattern2(int n){
         for(int i =0;i<n;i++){
             for(int j = 0;j< n;j++){
-                System.out.print("* ");
+                System.out.print("*");
             }
             System.out.println();
         }
@@ -51,4 +57,44 @@ public class PatternPrograms {
             System.out.println();
         }
     }
+    public static void pattern5(int n ){
+        for(int row = 1;row < 2*n ;row++){
+            int coltoPrint = row > n ? 2* n-row : row;
+            for(int j =1 ; j  <= coltoPrint;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern6(int n ){
+        for(int row = 1;row < 2*n ;row++){
+            int coltoPrint = row > n ? 2* n-row : row;
+            int NumberOfSpace = n - coltoPrint;
+            for(int i = 0;i<NumberOfSpace;i++)
+            {
+                System.out.print(" ");
+            }
+            for(int j =1 ; j  <= coltoPrint;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern7(int n ){
+        for(int row = 1 ;row<= n;row++)
+        {
+            for (int space = 0; space < n - row ; space++) {
+                System.out.print(" ");
+            }
+            for (int col = row; col >=1 ; col--) {
+                System.out.print(col);
+            }
+            for (int col = 2; col <= row ; col++) {
+                System.out.print(col);
+
+            }
+            System.out.println();
+        }
+    }
+
 }
